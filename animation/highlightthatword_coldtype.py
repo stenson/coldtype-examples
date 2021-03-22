2e4284bf989997ad7bdcc4990ec09ab2c1c6a279
Rob Stenson <rob.stenson@gmail.com>
2021-03-22 10:44:57 -0700

    use richtext from drafting in coldtype version

--- animation/highlightthatword_coldtype.py
+++ animation/highlightthatword_coldtype.py
@@ -1,28 +1,30 @@
 from coldtype import *
-from coldtype.text.richtext import RichText
-
-#f1 = Font.Cacheable("/System/Library/Fonts/SFNSTextCondensed-Bold.otf")
-#f2 = Font.Cacheable("/System/Library/Fonts/SFNSTextCondensed-Heavy.otf")
+from drafting.text.richtext import RichText
 
 f1 = Font.Cacheable("~/Type/fonts/fonts/ObviouslyVariable.ttf")
 tl = Timeline(30)
 
 @animation((1000, 500), timeline=tl)
 def highlight(f):
+    e = f.a.progress(f.i, loops=1, easefn="eeio").e
     rt = (RichText(f.a.r,
         "HIGHLIGHT[h]\nTHAT WORD",
         {
             "default": Style(f1, 150,
                 wght=0.25,
-                wdth=0.15-f.a.progress(f.i, loops=1, easefn="eeio").e*0.15, fill=bw(0.2)),
+                wdth=0.15-e*0.15,
+                fill=bw(0.2),
+                slnt=e),
             "h": Style(f1, 150,
                 ro=1,
                 wdth=0.25,
-                wght=f.a.progress(f.i, loops=1, easefn="eeio").e, fill=hsl(0.9, 0.8, 0.8),
+                wght=e,
+                slnt=1-e,
+                fill=hsl(e, 0.8, 0.8),
                 stroke=hsl(0.55, 0.7),
                 strokeWidth=5)})
         .xa()
         .align(f.a.r))
+    
     rt[-1].rotate(2)
-    #print(rt.tree()) # structured representation
     return rt
